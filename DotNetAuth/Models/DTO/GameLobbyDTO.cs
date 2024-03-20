04c58697cc0bd26bd088b2073ae9d2c12be9812a
Thijmen Tang <thijmtang2003@gmail.com>
2024-03-20 19:45:20 +0100

    Improves
    - Added Game lobby

--- DotNetAuth/Models/DTO/GameLobbyDTO.cs
+++ DotNetAuth/Models/DTO/GameLobbyDTO.cs
@@ -1,14 +1,60 @@
 ﻿namespace DotNetAuth.Models.DTO
 {
+    public enum STATUS {
+        STANDBY,
+        ONGOING,
+        COMPLETED,
+    }
+
     public class GameLobbyDTO
     {
-        public string Player1 { get; set; }
-        public string Player2 { get; set; }
+        public STATUS status { get; set; } = new();
 
-        public string CurrentPlayerTurn { get; set; }
+        private Dictionary<string, GamePlayerDto> Players { get; set; } = new();
 
-        public List<GameCell> GameField { get; set; }
+        public GamePlayerDto CurrentPlayerTurn { get; set; } = new();
 
+        public List<GameCell> GameField { get; set; } = new();
+
+
+
+        public GameLobbyDTO()
+        {
+            this.status = STATUS.STANDBY;
+        }
+
+        public void RemovePlayer(string connectionId)
+        {
+            if (!Players.ContainsKey(connectionId))
+            {
+                throw new ArgumentException("Speler bestaat niet");
+            }
+
+
+            Players.Remove(connectionId);
+
+        }
+
+        public void AddPlayer(GamePlayerDto player)
+        {
+            if (Players.ContainsKey(player.ConnectionId))
+            {
+                throw new ArgumentException("Speler zit al in de lobby!");
+            }
+
+            if (Players.Count >= 2)
+            {
+                throw new Exception("Er mogen maximaal 2 spelers in een lobby zitten");
+            }
+
+
+            Players.Add(player.ConnectionId, player);
+        }
+
+        public Dictionary<string, GamePlayerDto> getPlayers()
+        {
+            return Players;
+        }
 
     }
 }
